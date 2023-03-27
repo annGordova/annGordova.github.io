a393571d72123244c1c5145c5e584efc5022965f
annGordova <113303513+annGordova@users.noreply.github.com>
2023-03-27 20:38:43 +0300

    Update index.php

--- Task3app/index.php
+++ Task3app/index.php
@@ -68,8 +68,7 @@ try {
   }
   foreach ($_POST['abilities'] as $ability) {
     print($ability);
-    $stmt = $db->prepare("INSERT INTO sposob SET tip = ?");
-    print($stmt);
+    $stmt = $db->prepare("INSERT INTO sposob SET tip = ''' ? ''' ");
     $stmt->execute([$_POST['$ability']]);}
 }
 catch(PDOException $e){
