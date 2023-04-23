963f9ad8afc9e8cb1b8ba5cf67dd67b64e86ae9b
annGordova <113303513+annGordova@users.noreply.github.com>
2023-04-23 18:35:01 +0300

    Update form.php

--- Task4app/form.php
+++ Task4app/form.php
@@ -81,7 +81,7 @@
         <label>
             Биография и пожаления:<br />
             
-            <textarea name="bio" <?php if ($errors['bio']) {print 'class="error"';} ?> value="<?php print $values['bio']; ?>" />
+            <textarea name="bio" <?php if ($errors['bio']) {print 'class="error"';} ?> value="<?php print $values['bio']; ?>" ></textarea>
         </label><br />
       </li>
       <li>
