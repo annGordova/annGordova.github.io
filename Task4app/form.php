bd728c543e54b127ce0218142f49575796781bdc
annGordova <113303513+annGordova@users.noreply.github.com>
2023-04-23 18:41:27 +0300

    Update form.php

--- Task4app/form.php
+++ Task4app/form.php
@@ -53,7 +53,7 @@
             Пол: <br />
             <label><input type="radio" name="gender" value = "2" checked="checked" <?php print($errors['gender'] ? 'class="error"' : '');?>
       <?php if ($values['gender']=='m') print 'checked';?>/> женский </label>
-            <label><input type="radio" name="gender" value = "2" checked="checked" <?php print($errors['gender'] ? 'class="error"' : '');?>
+            <label><input type="radio" name="gender" value = "2" <?php print($errors['gender'] ? 'class="error"' : '');?>
       <?php if ($values['gender']=='m') print 'checked';?>/> мужской </label> <br />
         </label>
       </li>
