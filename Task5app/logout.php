57bb896e0fe0011c9017ce2c4831d680c1eb5f96
annGordova <113303513+annGordova@users.noreply.github.com>
2023-04-30 15:44:43 +0300

    Update logout.php

--- Task5app/logout.php
+++ Task5app/logout.php
@@ -2,7 +2,7 @@
 
 
 header('Content-Type: text/html; charset=UTF-8');
-
+$GLOBALS['b'] = False;
 session_start();
 session_destroy();
 print('You have been logged out. <a href="index.php">Go back</a>');
