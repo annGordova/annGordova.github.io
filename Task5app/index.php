41d182c182c088daa7e4fdc0a5cdb3b1ea9c6487
annGordova <113303513+annGordova@users.noreply.github.com>
2023-04-30 11:56:18 +0300

    Update index.php

--- Task5app/index.php
+++ Task5app/index.php
@@ -107,7 +107,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     // и заполнить переменную $values,
     // предварительно санитизовав.
     $db = foo();
-    $stmt = $db->prepare("SELECT l.login, z.namee, z.email, z.godrod, z.pol, z.konech, z.biogr FROM lopata l, zayava z WHERE l.login = $_SESSION['login']");
+    $stmt = $db->prepare("SELECT l.login, z.namee, z.email, z.godrod, z.pol, z.konech, z.biogr FROM lopata l, zayava z WHERE l.login = '1876'");
     //$stmt->execute();
     if($result = $stmt->execute()){
       foreach($result as $row){
