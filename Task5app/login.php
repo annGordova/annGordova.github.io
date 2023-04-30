116dad6e72b1847d85605dddd91f846aaf0ef897
annGordova <113303513+annGordova@users.noreply.github.com>
2023-04-30 14:38:45 +0300

    Update login.php

--- Task5app/login.php
+++ Task5app/login.php
@@ -28,7 +28,7 @@ if (!empty($_SESSION['login'])) {
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 ?>
 
-<form action="" method="post">
+<form action="index.php" method="post">
   <input name="login" />
   <input name="pass" />
   <input type="submit" value="Войти" />
