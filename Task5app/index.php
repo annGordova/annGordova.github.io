6c4b32e7f129864a721eaea6c907772ac1cf6bfe
annGordova <113303513+annGordova@users.noreply.github.com>
2023-04-30 11:50:52 +0300

    fgh

--- Task5app/index.php
+++ Task5app/index.php
@@ -106,6 +106,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     // TODO: загрузить данные пользователя из БД
     // и заполнить переменную $values,
     // предварительно санитизовав.
+    $db = foo();
+    $stmt = $db->prepare("SELECT l.login, z.namee, z.email, z.godrod, z.pol, z.konech, z.biogr FROM lopata l, zayava z WHERE l.login = $_POST['login']");
+    //$stmt->execute();
+    if($result = $stmt->execute()){
+      foreach($result as $row){
+        $values['name']= $row["namee"];
+        $values['email'] = $row["email"];
+        $values['year'] = $row["godrod"];
+        $values['kon'] = $row["konech"];
+        $values['gender'] = $row["pol"];
+        $values['bio'] = $row["biogr"];
+
+      }
+    }       
+ 
+    
+
     printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
   }
 
